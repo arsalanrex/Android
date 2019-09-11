b3891a2c1bd04f99f08bbbe144a7fb7c58110818
arsalanrex <arsalan.rex@gmail.com>
2019-09-11 10:54:55 +0530

    TextView

--- app/src/main/java/com/project/arsalan/androidstudioprojects/MainActivity.java
+++ app/src/main/java/com/project/arsalan/androidstudioprojects/MainActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView t;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView t1 = (TextView)findViewById(R.id.t1);
+        TextView textView = (TextView)findViewById(R.id.textView1);
     }
 }
