3745badfb9a03825671d017b59ae3a075e4433a7
Arsalan <31470152+arsalanrex@users.noreply.github.com>
2019-09-11 11:02:08 +0530

    Update MainActivity.java

--- app/src/main/java/com/project/arsalan/androidstudioprojects/MainActivity.java
+++ app/src/main/java/com/project/arsalan/androidstudioprojects/MainActivity.java
@@ -1,8 +1,8 @@
+//new android project
 package com.project.arsalan.androidstudioprojects;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView t;
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView t1 = (TextView)findViewById(R.id.t1);
     }
 }
