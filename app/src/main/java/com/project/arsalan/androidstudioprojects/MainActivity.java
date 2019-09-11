97e23f95f35fd9ae54b06985825031d294c4e295
arsalanrex <arsalan.rex@gmail.com>
2019-09-11 10:00:26 +0530

    EditText

--- app/src/main/java/com/project/arsalan/androidstudioprojects/MainActivity.java
+++ app/src/main/java/com/project/arsalan/androidstudioprojects/MainActivity.java
@@ -2,15 +2,40 @@ package com.project.arsalan.androidstudioprojects;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView t;
+    EditText editText;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView t1 = (TextView)findViewById(R.id.t1);
+        final EditText editText = (EditText)findViewById(R.id.editText1);
+        final TextView textView = (TextView)findViewById(R.id.textView1);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String str = editText.getText().toString();
+                textView.setText(str);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
